a4b136f53ff140b4065cbdd1571d8bd241c16c19
Kyle Marx <Kyle Marx>
2016-09-14 10:23:54 -0700

    js skeleton

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    console.log('document has loaded');
+});
+
+//build board dynamically based on chosen board size (visually)
+//build empty array to place markers in
+
+//add a single marker to the game board (visually)
+//add a single marker to the game array
+
+//switch turn to the next player
+
+//check for winning combinations
+
+//end game: stop all movements, initiate end game screen
+
+//retire from game: go to home screen
