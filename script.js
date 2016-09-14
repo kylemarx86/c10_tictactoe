13ccee655e900191e3af94fa299dc65a5766b6a9
bournecreative <bournecreative@gmail.com>
2016-09-14 13:04:23 -0700

    version0 dynamically created 3X3 with click event

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+//this sets up the game board with a 3x3 game only
 $(document).ready(function(){
-    console.log('document has loaded');
+  create_game_board();
+  player_make_move();
 });
+
+
+function create_game_board() {
+  var game_board_size = 3;
+  
+  for (var a = 0; a<game_board_size; a++) {
+    var row_one = $('<div>', {id: "row0"+" "+a}).text("index" + a).addClass('mark_spot');
+    $('.game_board').append(row_one);
+  }
+  for (var b = 0; b<game_board_size; b++) {
+    var row_two = $('<div>', {id: "row1"+" "+b}).text("index" + b).addClass('mark_spot');
+    $('.game_board').append(row_two);
+  }
+  for (var c = 0; c<game_board_size; c++) {
+    var row_three = $('<div>', {id: "row2"+" "+c}).text("index" + c).addClass('mark_spot');
+    $('.game_board').append(row_three);
+  }
+}
+
+function player_make_move(){
+  $('.mark_spot').click(area_checked);
+}
+
+function area_checked(){
+  console.log("Clicked");
+}
+
+
+
 
 //build board dynamically based on chosen board size (visually)
 //build empty array to place markers in
