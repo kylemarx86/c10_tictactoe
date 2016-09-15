14a7376bd5ea5be22f7567bcf18e9c2bcac92027
bournecreative <bournecreative@gmail.com>
2016-09-15 11:16:37 -0700

    Pulled script.js from master and commiting new index and css

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 var game_board_size = null;
 var stored_game_data = null;
 var its_player_ones_turn = null;
-var player_ones_turn = true;
+
 
 $(document).ready(function(){
-  create_game_board();
-  player_make_move;
+    $("#new_game").click(function(){  //when the new game link is clicked, the game board is cleared of all elements.
+        $(".game_board").empty();   //Without this, the squares would continue to append on tp of each other each time a new game is started.
+    });
 });
 
 function create_game_board() {
     // game_board_size = 3;        //temporarily hard set game_board_size
-    size_of_board = 20;          //temporarily hard set to 3
+    // size_of_board = 5;          //temporarily hard set to 3
     stored_game_data = [];      //reset stored_game_data to blank array
     its_player_ones_turn = true;        //at start of the game player one will start
 
@@ -29,78 +30,65 @@ function create_game_board() {
     }
 }
 
+function player_make_move(){
+  $('.mark_spot').click(area_checked);
+}
 
+function area_checked(){
+  console.log("Clicked");
+    var row = $(this).attr('row');
+    var column = $(this).attr('column');
 
+    console.log('row', row);
+    console.log('column', column);
 
-/*
-function area_checked() {
-  var this_square = $(this).attr('row');
-  if (player_ones_turn === true) {
-    console.log("Clicked" + this_square);
-    stored_game_data[row][column] = 1;
-  }
-  */
-  /*console.log("Clicked");
-   var row = $(this).attr('row');
-   var column = $(this).attr('column');
-   
-   console.log('row', row);
-   console.log('column', column);
-   
-   if(stored_game_data[row][column] === null){     //check to see if the clicked cell is null/empty
-   if(its_player_ones_turn){
-   stored_game_data[row][column] = 1;    //if it is player ones turn put a one in the given cell of the game array
-   }else {
-   stored_game_data[row][column] = 2;       //if it is player twos turn put a 2 in the given cell of the game array
-   }
-   its_player_ones_turn = !its_player_ones_turn;       //if the person made a legitimate move then the players turn will switch
-   }else {
-   console.log('spot taken');      //not sure we need this case //leave it just in case there is something to account for
-   }
-   console.log(stored_game_data);
-   }
-   */
+    if(stored_game_data[row][column] === null){     //check to see if the clicked cell is null/empty
+        if(its_player_ones_turn){
+            stored_game_data[row][column] = 1;    //if it is player ones turn put a one in the given cell of the game array
+        }else {
+            stored_game_data[row][column] = 2;       //if it is player twos turn put a 2 in the given cell of the game array
+        }
+        its_player_ones_turn = !its_player_ones_turn;       //if the person made a legitimate move then the players turn will switch
+    }else {
+        console.log('spot taken');      //not sure we need this case //leave it just in case there is something to account for
+    }
+
+    console.log(stored_game_data);
+}
 
 //set game board size
+//game board size parameter is passed when the player clicks on the game board selection screen
 function set_game_board_size(size) {
     size_of_board = size;
 }
 
 //build board dynamically based on chosen board size (visually)
 
-//choose random win_length - for boards larger than 3x3
-
-function num_of_winning_matches_needed() {
-    //3 board size variables (booleans) created to test this function
-    //var board_20 isn't actually needed, as it would be caught by the "else" statement
-    var board_ = false;
-    var board_9 = true;
-    var board_20 = false;
+//choose random # of matches needed to win
+// function is called when the board size is selected.
+// for boards larger than 3x3
+function num_of_winning_matches_needed(size) {
     //if board is 3x3, # of matches needed to win will be 3
-    if (board_3) {
-        var winning_matches_3_board = 3;
-        console.log("number of matches needed for 3x3 board: "+winning_matches_3_board);
-        return winning_matches_3_board;
+    if (size === 3) {
+        winning_matches = 3;
+        console.log("number of matches needed for 3x3 board: "+winning_matches);
     }
     //if board is 9x9
     //Math.Random gives random # from 0 - .9999[...]
     // Math.floor gives whole number, *3 + 3 multiplies whole number by 6 and adds 3 (minimum # of matches needed are 3).
-    else if (board_9) {
-        var winning_matches_9_board = Math.floor(Math.random() * 6 + 4);
-        console.log("number of matches needed for 9x9 board: "+winning_matches_9_board);
-        return winning_matches_9_board;
+    else if (size === 9) {
+        winning_matches = Math.floor(Math.random() * 6 + 4);
+        console.log("number of matches needed for 9x9 board: "+winning_matches);
     }
-    //else if board is 20x20
+    //otherwise, for a 20x20 board,
     //Math.Random gives random # from 0 - .9999[...]
     // Math.floor gives whole number, *17 + 3 multiplies whole number by 17 and adds 3 (minimum # of matches needed are 3).
     else {
-        var winning_matches_20_board = Math.floor(Math.random() * 17 + 4);
-        console.log("number of matches needed for 3x3 board: "+winning_matches_20_board);
-        return winning_matches_20_board;
+        winning_matches = Math.floor(Math.random() * 17 + 4);
+        console.log("number of matches needed for 20x20 board: "+winning_matches);
     }
 }
-//calling the winning matches needed function:
-num_of_winning_matches_needed();
+
 
 
 //build empty array to place markers in
